2f79015e9c4776ee10383c4bf9845ba678299390
MartinLuczka <luczka.martin@gmail.com>
2024-04-28 03:53:07 +0200

    Úprava a rozšíření komentářů kódu

--- app.py
+++ app.py
@@ -2,8 +2,12 @@ from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
 from dbModels.Models import db
+from pyFiles.UserController import login
+
+# používané importy
 
 app = Flask(__name__)
+# vytvoření instance samotné Flask aplikace, Třída Flask je hlavním prvkem knihovny Flask, reprezentuje samotnou aplikaci
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 # hranatými závorkami vytváříme záznam v konfiguračním slovníku flask aplikace, konfigurace webové aplikace, zavádíme jazyk a název databáze
@@ -20,16 +24,21 @@ with app.app_context():
 @app.route("/")
 def home():
     return "Hello, Flask!"
+# domovská stránka webu
 
 @app.route("/prihlaseni", methods = ["GET", "POST"])
+# stránka řešící přihlášení
 def prihlaseni():
     infoMessage = ''
     if request.method == 'POST':
-
-        if login( request.form['username'] , request.form['password'] ):
-            return render_template( 'HomePage.html', webTitle='Domovská stránka' )
+    # Uživatel vyplnit formulář pro přihlášení
+        if login(request.form['username'] , request.form['password']):
+            return render_template('HomePage.html', webTitle='Domovská stránka')
+        # Přihlášení se podařilo, uživatel se dostane na domovskou stránku
         else:
             infoMessage = 'Wrong username or password'
+        # Přihlášení se nezdaří, informuje me ho o špatně zadaných údajích
 
-    return render_template( 'Login.html', webTitle='Přihlášení' , infoMessage=infoMessage)
+    return render_template('Login.html', webTitle='Přihlášení' , infoMessage=infoMessage)
+    # uživatel zůstane na stránce přihlášení, proměnnou infoMessage předáváme stránce Login.html
 
