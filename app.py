6304b036f612e7c7577e6f2d9ce155787deed45a
MartinLuczka <luczka.martin@gmail.com>
2024-04-28 17:51:06 +0200

    Přidání funkcionality registrace

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, session, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 from dbModels.Models import db
-from pyFiles.UserController import login
+from pyFiles.UserController import login, signup
 
 # používané importy
 
@@ -11,6 +11,9 @@ app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 # hranatými závorkami vytváříme záznam v konfiguračním slovníku flask aplikace, konfigurace webové aplikace, zavádíme jazyk a název databáze
+
+app.secret_key = "tajny_klic"
+# nastavení klíče pro session
 
 #print(app.config) - můžeme si zobrazit konfiguraci aplikace
 
@@ -23,7 +26,8 @@ with app.app_context():
 
 @app.route("/")
 def home():
-    return "Hello, Flask!"
+    print(session)
+    return render_template("HomePage.html", webTitle = "Domovská stránka")
 # domovská stránka webu
 
 @app.route("/prihlaseni", methods = ["GET", "POST"])
@@ -33,7 +37,7 @@ def prihlaseni():
     if request.method == 'POST':
     # Uživatel vyplnit formulář pro přihlášení
         if login(request.form['username'], request.form['password']):
-            return render_template('HomePage.html', webTitle='Domovská stránka')
+            return redirect("/")
         # Přihlášení se podařilo, uživatel se dostane na domovskou stránku
         else:
             infoMessage = 'Chybné heslo nebo přihlašovací jméno'
@@ -47,11 +51,21 @@ def prihlaseni():
 def odhlaseni():
     session.pop("user", None)
     # ukončení sessionu uživatele
-    redirect("/")
-    # po odhlášení přesměrování na domovskou stránku
+    return redirect("/")
+    # po odhlášení přesměrování na domovskou stránku, uživatel již není v session
 
 @app.route("/registrace", methods = ["GET", "POST"])
 # stránka pro registraci
 def registrace():
-
+    infoMessage = ""
+    if request.method == "POST":
+        signup_result = signup(request.form["username"], request.form["password"], request.form["password_confirm"])
+        # funkce vrací True nebo Error podle toho, jestli se uživatele podařilo zaregistrovat
+        if isinstance(signup_result, bool) and signup_result == True:
+            # vrátí True, pokud signup_result bude True (boolean hodnota)
+            return redirect("/prihlaseni")
+        else:
+            # pokud signup_result bude error, tak se přepíše do infoMessage
+            infoMessage = signup_result
+    return render_template("SignUp.html", webTitle = "Registrace", infoMessage = infoMessage)
 
--- pyFiles/UserController.py
+++ pyFiles/UserController.py
@@ -1,13 +1,15 @@
 from flask import request, session
 from sqlalchemy import text
-from Encoder import sha256
+from sqlalchemy.exc import SQLAlchemyError
+
+from pyFiles.Encoder import sha256
 from dbModels.Models import db
 
 def login(username, password):
     # ověření záznamu při přihlášení, vracíme True/False
     query = text("SELECT * FROM users WHERE username = :username AND password = :password")
     # SQL command v textu
-    parametres = {"username": request.form['username'], "password": sha256(request.form(password))}
+    parametres = {"username": request.form['username'], "password": sha256(request.form["password"])}
     # práce s databází
     user = db.session.execute(query, parametres).fetchone()
     # execute - parametry zadá to query (metoda sqlalchemy), fetchone() - získání jednoho řádku v databázi z výsledku metody (vrátí řádek nebo None)
@@ -18,3 +20,19 @@ def login(username, password):
         # přihlášení se podařilo
     return False
     # přihlášení se nezdařilo
+
+def signup(username, password, password_confirm):
+    if password != password_confirm:
+        return "Zadaná hesla se neshodují"
+    query = text("INSERT INTO users (username, password) VALUES (:username, :password)")
+    # SQL command - vložení záznamu do tabulky
+    parametres = {"username": username, "password": sha256(password)} # heslo zakódujeme pomocí funkce
+    # parametry pro SQL command (VALUES)
+    try:
+        db.session.execute(query, parametres)
+        db.session.commit()
+        # vložení hodnot do databáze
+    except SQLAlchemyError as error:
+        return error
+    # pokud nastane chyba při zapsání do databáze, tak se vykoná blok kódu except, vrácený error se zapíše do infoMessage
+    return True
